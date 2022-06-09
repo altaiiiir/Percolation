e66310b71f4db0edd955b685a3fca53bcf08c48f
Mohamed Alturfi <mohamed.alturfi.bashpole@gmail.com>
2022-06-09 13:59:41 -0700

    - Most methods implemented (not tested) currently on percolate

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -5,50 +5,87 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     // declare instance variables
-    int [][] grid;
+    private int [][] grid;
+    private int [][] load;
+    private int size;
+    private int openSites = 0;
+    private boolean percolates = false;
+    private WeightedQuickUnionUF qu;
 
-    // creates n-by-n grid, with all sites initially blocked
+    // creates n-by-n matrix, with all sites initially blocked
     public Percolation(int n){
-        // can't have a grid smaller than 1 x 1
+        // can't have a matrix smaller than 1 x 1
         if(n <= 0) throw new IllegalArgumentException();
 
-        // create a new 1 indexed 2d array
-        grid = new int[n+1][n+1];
+        this.size = n;
+        qu = new WeightedQuickUnionUF((this.size*this.size)+1);
 
-        // Initialize all sites to be blocked
+        // create a new 1 indexed 2d array & its respective load 2d array
+        this.grid = new int[this.size+1][this.size+1];
+        this.load = new int[this.size+1][this.size+1];
 
+        // Initialize all sites to be blocked on grid (0 = blocked 1 = open)
+        // Initialize all sites to be empty on load (0 = empty 1 = full)
+        for (int i = 1; i < this.grid.length; i++) {
+            for (int j = 1; j < this.grid[i].length; j++){
+                this.grid[i][j] = 0;
+                this.load[i][j] = 0;
+            }
+        }
+    }
+
+    private int flattenGrid(int row, int col) {
+        return this.size * (row - 1) + col;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
+        if(row<=0 || row>this.size || col<=0 || col>this.size) throw new IllegalArgumentException();
+        this.grid[row][col] = 1;
+        if(row+1 == 1) this.load[row][col] = 1;
+
+        // top
+        if(isOpen(row-1,col)) this.qu.union(flattenGrid(row,col), flattenGrid(row-1,col));
+        // down
+        if(isOpen(row+1,col)) this.qu.union(flattenGrid(row,col), flattenGrid(row+1,col));
+        // left
+        if(isOpen(row,col-1)) this.qu.union(flattenGrid(row,col), flattenGrid(row,col-1));
+        // right
+        if(isOpen(row,col+1)) this.qu.union(flattenGrid(row,col), flattenGrid(row,col+1));
+
+        //if(this.qu.connected(flattenGrid(row,col), flattenGrid(row,col))
 
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
-
-        return false;
+        if(row<=0 || row>size || col<=0 || col>size) throw new IllegalArgumentException(); this.openSites++;
+        return this.grid[row][col] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col){
-
-        return false;
+        if(row<=0 || row>this.size || col<=0 || col>this.size) throw new IllegalArgumentException();
+        return this.load[row][col] == 1;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites(){
-
-        return 0;
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates(){
-
         return false;
     }
 
     public static void main(String[] args){
+        Percolation perc = new Percolation(10);
 
+        for (int i = 1; i < perc.grid.length; i++) {
+            for (int j = 1; j < perc.grid[i].length; j++)
+                System.out.print(perc.grid[i][j] + " ");
+            System.out.println();
+        }
     }
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -3,4 +3,72 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
+    // Instance Variables
+    private int numOpened = 0;
+
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials){
+        if(n<=0 || trials<=0) throw new IllegalArgumentException();
+
+        // Monty Carlos Sim
+        Percolation perc = new Percolation(n);
+
+        for(int i = 0; i < trials; i++) {
+            // while(!perc.percolates()||
+            for(int j = 0; j < trials; j++) {
+                // choose and open a random (closed) site
+                int randX, randY = 0;
+                do {
+                    randX = StdRandom.uniform(1, n + 1);
+                    randY = StdRandom.uniform(1, n + 1);
+                } while (perc.isOpen(randX, randY));
+                perc.open(randX, randY);
+                numOpened++;
+            }
+        }
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+
+        return 0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+
+        return 0;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo(){
+
+        return 0;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+
+        return 0;
+    }
+
+    // test client (see below)
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        PercolationStats percs = new PercolationStats(n, trials);
+
+        System.out.print("mean");
+        for (int i = 4; i < 26; i++) System.out.print(" ");
+        System.out.println("= " + percs.numOpened);
+        System.out.print("stddev");
+        for (int i = 6; i < 26; i++) System.out.print(" ");
+        System.out.println("= ");
+        System.out.print("95% confidence interval");
+        for (int i = 23; i < 26; i++) System.out.print(" ");
+        System.out.println("= ");
+    }
 }
